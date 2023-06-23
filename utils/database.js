1a0bf7c7cd7f6ad65569fbb5bb1fa4b9aa6b2cca
pawanpdn-671 <pawanpradhanpc671@gmail.com>
2023-06-23 19:15:47 +0530

    creating model from mongoose

---
+++ utils/database.js
@@ -0,0 +1,23 @@
+import mongoose, { mongo } from "mongoose";
+let isConnected = false; //tracking the connection
+
+export const connectToDB = async () => {
+	mongoose.set("strictQuery", true);
+
+	if (isConnected) {
+		console.log("MongoDB is already connected");
+		return;
+	}
+
+	try {
+		await mongoose.connect(process.env.MONGODB_URL, {
+			dbName: "share_prompt",
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		});
+		isConnected = true;
+		console.log("mongodb is connected");
+	} catch (error) {
+		console.log(error);
+	}
+};
